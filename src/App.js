558dd1d2d1268baf5bd21e5408ffe855125774d0
Tenzin <tenthinten@gmail.com>
2023-11-15 15:48:41 +0530

    first box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="container1">
+          <div className="container2">
+            <div className="first">
+              <div className="profile">
+                <div className="content">
+                  <div className="info">
+                    <img className="img" src="" alt="" />
+                    <div className="personal">
+                      <div className="name">Daniel Clifford</div>
+                      <div className="qualification">Verified Graduate</div>
+                    </div>
+                  </div>
+                        <h1>
+                          I received a job offer mid-course, and the subjects I learned
+                          were current, if not more so, in the company I joined. I
+                          honestly feel I got every penny’s worth.
+                        </h1>
+                        <p>
+                          “ I was an EMT for many years before I joined the bootcamp. I’ve
+                          been looking to make a transition and have heard some people who
+                          had an amazing experience here. I signed up for the free intro
+                          course and found it incredibly fun! I enrolled shortly
+                          thereafter. The next 12 weeks was the best - and most grueling -
+                          time of my life. Since completing the course, I’ve successfully
+                          switched careers, working as a Software Engineer at a VR
+                          startup. ”
+                        </p>
+                </div>
+              </div>
+            </div>
+            <div className="second"></div>
+          </div>
+          <div className="container3">
+            <div className="third"></div>
+            <div className="fourth"></div>
+          </div>
+        </div>
+        <div className="fifth"></div>
+      </div>
     </div>
   );
 }
