c9d0b2ac9f08af41a5e117ec0fcd71a8898ede33
Tenzin <tenthinten@gmail.com>
2023-11-21 14:58:30 +0530

    desktop version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import "./App.css";
+import quote from "./images/bg-pattern-quotation.svg";
+import daniel from "./images/image-daniel.jpg";
+import Jonathan from "./images/image-jonathan.jpg";
+import Jeanette from "./images/image-jeanette.jpg";
+import Patrick from "./images/image-patrick.jpg";
+import Kira from "./images/image-kira.jpg";
 
 function App() {
   return (
@@ -10,38 +16,130 @@ function App() {
               <div className="profile">
                 <div className="content">
                   <div className="info">
-                    <img className="img" src="" alt="" />
+                    <img className="img1" src={daniel} alt="daniel" />
                     <div className="personal">
                       <div className="name">Daniel Clifford</div>
                       <div className="qualification">Verified Graduate</div>
                     </div>
                   </div>
-                        <h1>
-                          I received a job offer mid-course, and the subjects I learned
-                          were current, if not more so, in the company I joined. I
-                          honestly feel I got every penny’s worth.
-                        </h1>
-                        <p>
-                          “ I was an EMT for many years before I joined the bootcamp. I’ve
-                          been looking to make a transition and have heard some people who
-                          had an amazing experience here. I signed up for the free intro
-                          course and found it incredibly fun! I enrolled shortly
-                          thereafter. The next 12 weeks was the best - and most grueling -
-                          time of my life. Since completing the course, I’ve successfully
-                          switched careers, working as a Software Engineer at a VR
-                          startup. ”
-                        </p>
+                  <img className="quote" src={quote} alt="big-quote" />
+                  <h1>
+                    I received a job offer mid-course, and the subjects I
+                    learned were current, if not more so, in the company I
+                    joined. I honestly feel I got every penny’s worth.
+                  </h1>
+
+                  <p>
+                    “ I was an EMT for many years before I joined the bootcamp.
+                    I’ve been looking to make a transition and have heard some
+                    people who had an amazing experience here. I signed up for
+                    the free intro course and found it incredibly fun! I
+                    enrolled shortly thereafter. The next 12 weeks was the best
+                    - and most grueling - time of my life. Since completing the
+                    course, I’ve successfully switched careers, working as a
+                    Software Engineer at a VR startup. ”
+                  </p>
                 </div>
               </div>
             </div>
-            <div className="second"></div>
+
+            <div className="second">
+              <div className="profile">
+                <div className="content">
+                  <div className="info">
+                    <img className="img1" src={Jonathan} alt="daniel" />
+                    <div className="personal">
+                      <div className="name">Jonathan Walters</div>
+                      <div className="qualification">Verified Graduate</div>
+                    </div>
+                  </div>
+                  <h1>The team was very supportive and kept me motivated</h1>
+                  <p>
+                    “ I started as a total newbie with virtually no coding
+                    skills. I now work as a mobile engineer for a big company.
+                    This was one of the best investments I’ve made in myself. “
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
+
           <div className="container3">
-            <div className="third"></div>
-            <div className="fourth"></div>
+            <div className="third">
+              <div className="profile">
+                <div className="content">
+                  <div className="info">
+                    <img className="img1" src={Jeanette} alt="daniel" />
+                    <div className="personal">
+                      <div className="name">Jeanette Harmon</div>
+                      <div className="qualification">Verified Graduate</div>
+                    </div>
+                  </div>
+                  <h1>An overall wonderful and rewarding experience</h1>
+                  <p>
+                    “ Thank you for the wonderful experience! I now have a job I
+                    really enjoy, and make a good living while doing something I
+                    love. ”
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="fourth">
+              <div className="profile">
+                <div className="content">
+                  <div className="info">
+                    <img className="img1" src={Patrick} alt="daniel" />
+                    <div className="personal">
+                      <div className="name">Patrick Abrams</div>
+                      <div className="qualification">Verified Graduate</div>
+                    </div>
+                  </div>
+                  <h1>
+                    Awesome teaching support from TAs who did the bootcamp
+                    themselves. Getting guidance from them and learning from
+                    their experiences was easy.
+                  </h1>
+                  <p>
+                    “ The staff seem genuinely concerned about my progress which
+                    I find really refreshing. The program gave me the confidence
+                    necessary to be able to go out in the world and present
+                    myself as a capable junior developer. The standard is above
+                    the rest. You will get the personal attention you need from
+                    an incredible community of smart and amazing people. ”
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="fifth"></div>
+        <div className="fifth">
+          <div className="profile">
+            <div className="content">
+              <div className="info">
+                <img className="img1" src={Kira} alt="daniel" />
+                <div className="personal">
+                  <div className="name">Kira Whittle</div>
+                  <div className="qualification">Verified Graduate</div>
+                </div>
+              </div>
+              <h1>Such a life-changing experience. Highly recommended!</h1>
+              <p>
+                “ Before joining the bootcamp, I’ve never written a line of
+                code. I needed some structure from professionals who can help me
+                learn programming step by step. I was encouraged to enroll by a
+                former student of theirs who can only say wonderful things about
+                the program. The entire curriculum and staff did not disappoint.
+                They were very hands-on and I never had to wait long for
+                assistance. The agile team project, in particular, was
+                outstanding. It took my learning to the next level in a way that
+                no tutorial could ever have. In fact, I’ve often referred to it
+                during interviews as an example of my developent experience. It
+                certainly helped me land a job as a full-stack developer after
+                receiving multiple offers. 100% recommend! ”
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
